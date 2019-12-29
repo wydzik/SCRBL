67e7225963bcf4c78b3a51665743b2cda148a93b
Wydzik <l.gryinternet@gmail.com>
2019-12-29 12:13:48 +0100

    try

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: daphne betterScrabble.asgi:channel_layer --port $PORT --bind 0.0.0.0 -v2
+web: daphne django_channels_heroku.asgi:application --port $PORT --bind 0.0.0.0
 worker: python manage.py runworker -v2
--- betterScrabble/routing.py
+++ betterScrabble/routing.py
@@ -1,5 +1,5 @@
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 import main.routing
 
 application = ProtocolTypeRouter({
