76430093c05d0e8a02fe747b4ea3969fad26929f
Wydzik <l.gryinternet@gmail.com>
2019-12-29 12:35:08 +0100

    try

--- betterScrabble/asgi.py
+++ betterScrabble/asgi.py
@@ -1,6 +1,5 @@
-import os
 from channels.routing import get_default_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "betterScrabble.settings")
+import django
+django.setup()
 
 application = get_default_application()
--- betterScrabble/settings.py
+++ betterScrabble/settings.py
@@ -13,9 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import django_heroku
-import django
-django.setup()
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
