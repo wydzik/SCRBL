dd1421976b472b4d03aa956d7f125aaf07e50acf
Wydzik <l.gryinternet@gmail.com>
2019-12-29 12:43:20 +0100

    try

--- betterScrabble/settings.py
+++ betterScrabble/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'channels'
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
