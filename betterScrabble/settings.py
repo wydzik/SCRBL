4aed83cc3b2a138f57bcaf7856392097b030f35a
Wydzik <l.gryinternet@gmail.com>
2019-12-29 12:46:58 +0100

    try

--- betterScrabble/settings.py
+++ betterScrabble/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = ['nameless-mountain-65773.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'channels',
+    'main'
 ]
 
 MIDDLEWARE = [
