ceac2f2de5e9f5866699a184241b025a1289968f
Wydzik <l.gryinternet@gmail.com>
2019-12-29 12:07:56 +0100

    try

---
+++ betterScrabble/asgi.py
@@ -0,0 +1,5 @@
+import os
+from channels.routing import get_default_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "betterScrabble.settings")
+application = get_default_application()
