4c0f4504b468a16db2bcd4c0cb70a22c939a6a7e
Wydzik <l.gryinternet@gmail.com>
2019-12-29 12:20:57 +0100

    try

--- betterScrabble/asgi.py
+++ betterScrabble/asgi.py
@@ -1,5 +1,7 @@
 import os
 from channels.routing import get_default_application
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "betterScrabble.settings")
+django.setup()
 application = get_default_application()
