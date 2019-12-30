374d0903700cbfe3ac4d70a2e5d8c7460122c7ee
Wydzik <l.gryinternet@gmail.com>
2019-12-30 20:20:14 +0100

    try

--- main/consumers.py
+++ main/consumers.py
@@ -1,13 +1,20 @@
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 from .models import Move, GameRooms, Profile
-from django.contrib.auth.models import User
 import json
 
 
 class GameConsumer(WebsocketConsumer):
     def connect(self):
-        self.accept()
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'game_%s' % self.room_name
 
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        self.accept()
 
     def disconnect(self, close_code):
         pass
@@ -18,11 +25,11 @@ class GameConsumer(WebsocketConsumer):
         points = text_data_json['points']
         player = text_data_json['player']
         gameroom = text_data_json['gameroom']
-        game_room = GameRooms.objects.get(pk = gameroom)
-        Move.objects.create(game_room = game_room, player = player, points = points, board_state = board_state)
+        game_room = GameRooms.objects.get(pk=gameroom)
+        Move.objects.create(game_room=game_room, player=player, points=points, board_state=board_state)
         # tutaj trzeba zrobić jakieś casy w zależności od tego, czy wszyscy zrobili ruch, czy nie, bo nie wyobrażam sobie tego inaczej
         # trzeba by chyba też jakiś mechanizm dołączania do gry zrobić i wtedy by się ten model Game nadał
         self.send(text_data=json.dumps({
             'boardState': 'NOT YET',
-            'points' : points
+            'points': points
         }))
