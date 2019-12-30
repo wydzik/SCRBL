0e4c7001068442a465320674566206c6fd3aafcc
Wydzik <l.gryinternet@gmail.com>
2019-12-30 23:28:03 +0100

    try

--- main/models.py
+++ main/models.py
@@ -40,6 +40,9 @@ class GameRooms(models.Model):
                                                            ",,,,,,,,,,,,,,," \
                                                            ",,,,,,,,,,,,,,,")
 
+class Game(models.Model):
+    game_room = models.ForeignKey(GameRooms, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
 
 class Move(models.Model):
     game_room = models.ForeignKey(GameRooms, on_delete=models.CASCADE)
