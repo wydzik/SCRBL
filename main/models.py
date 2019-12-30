94dc804d2042f36b31ae90a21f5215fa8c2d75fc
Wydzik <l.gryinternet@gmail.com>
2019-12-30 15:31:37 +0100

    try

--- main/models.py
+++ main/models.py
@@ -43,7 +43,7 @@ class GameRooms(models.Model):
 
 class Move(models.Model):
     game_room = models.ForeignKey(GameRooms, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    player = models.CharField(default=None)
     board_state = models.CharField(max_length=450, default=",,,,,,,,,,,,,,," \
                                                            ",,,,,,,,,,,,,,," \
                                                            ",,,,,,,,,,,,,,," \
