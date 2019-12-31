19f0357dd69ddb070c65b07c84e085b23a99413a
Wydzik <l.gryinternet@gmail.com>
2019-12-31 02:14:41 +0100

    try

--- main/consumers.py
+++ main/consumers.py
@@ -30,6 +30,7 @@ class GameConsumer(WebsocketConsumer):
         player = text_data_json['player']
         gameroom = text_data_json['gameroom']
         game_room = GameRooms.objects.get(pk=gameroom)
+
         if board_state == 'READY':
             round = text_data_json['round']
             user = User.objects.get(username=player)
@@ -37,6 +38,7 @@ class GameConsumer(WebsocketConsumer):
             self.send(text_data=json.dumps({
                 'boardState': 'WAITING_FOR_START'
             }))
+
             if len(Game.objects.filter(game_room=game_room)) == game_room.seats:
                 async_to_sync(self.channel_layer.group_send)(
                     self.room_group_name,
@@ -57,24 +59,30 @@ class GameConsumer(WebsocketConsumer):
                 'boardState': 'NOT YET',
                 'points': points
             }))
-            if len(Move.objects.filter(game_room=game_room, round=round)) == game_room.seats :
+            moves = Move.objects.filter(game_room=game_room, round=round)
+            if len(moves) == game_room.seats:
+                winner = moves.order_by('points')[-1]
+                board_state = winner.board_state
 
                 async_to_sync(self.channel_layer.group_send)(
                     self.room_group_name,
                     {
                         'type': 'move_info',
                         'boardState': board_state,
-                        'round': (round+1)
+                        'round': (round+1),
+                        'roundWinner': winner.player
                     }
                 )
 
     def move_info(self, event):
         board_state = event['boardState']
         round = event['round']
+        round_winner = event['roundWinner']
         # Send message to WebSocket
         self.send(text_data=json.dumps({
             'boardState': board_state,
-            'round': round
+            'round': round,
+            'roundWinner': round_winner
         }))
 
         # self.send(text_data=json.dumps({
