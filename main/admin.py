eea4e92962bcaab94e1c157da2f56f7f8c1e9313
Wydzik <l.gryinternet@gmail.com>
2020-01-02 12:52:25 +0100

    try

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import GameRooms, Move
+from .models import GameRooms, Move, Game
 
 admin.site.register(GameRooms)
 admin.site.register(Move)
+admin.site.register(Game)
